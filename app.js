f381ed4d170888c4d74122ee9a468aaeec255ab4
yovorichard3-lgtm <yovorichard3@gmail.com>
2026-03-04 11:09:14 +0100

    theme

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function applyTheme(theme) {
     document.documentElement.classList.toggle("theme-light", theme === "light")
     const btnTheme = document.querySelector("#btn-theme")
-    btnTheme.textContent = theme === "light" ? "obscure" : "claire"
+    const icon = btnTheme.querySelectorAll('> i')[0]
+
+    icon.dataset.lucide = theme === "light" ? "moon" : "sun"
 }
 
  
